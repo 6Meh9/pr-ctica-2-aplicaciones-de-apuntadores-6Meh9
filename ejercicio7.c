4ddff78a7e9b71b316baa71b34b3dcb4282b566b
Leonardo Nuñez Navarro <leonardo.nunez.12515@gmail.com>
2025-03-01 23:57:30 -0600

    Update ejercicio7.c

--- ejercicio7.c
+++ ejercicio7.c
@@ -4,5 +4,8 @@
 int main() {
     int arr[5] = {10, 20, 30, 40, 50};
     // Código para imprimir direcciones de memoria aquí
+    for (int i=0;i<5;i++){
+        printf("la direccion de memoria del elemento %i es: %p \n", arr[i], &arr[i]);
+    }
     return 0;
 }
